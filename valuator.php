4866d69b122491071ec58be2be2cd8dcd82194d3
Aaron Huisinga <aaronhuisinga@gmail.com>
2014-10-11 00:14:32 -0500

    Update plugin information; Bump version number

--- valuator.php
+++ valuator.php
@@ -1,10 +1,10 @@
 <?php
 /*
  * Plugin Name: Valuator
- * Version: 1.0
- * Plugin URI: http://www.coldturkrygroup.com/
- * Description: Home Valuator plugin that creates landing pages and allows users to get data from Zillow API.
- * Author: Aaron Huisinga
+ * Version: 1.1.2
+ * Plugin URI: http://www.coldturkeygroup.com/
+ * Description: Home Valuation plugin that creates landing pages and allows visitors to get valuation data from Zillow API.
+ * Author: Cold Turkey Group
  * Author URI: http://www.coldturkeygroup.com/
  * Requires at least: 4.0
  * Tested up to: 4.0
@@ -22,15 +22,12 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 if ( ! defined( 'VALUATOR_PLUGIN_PATH' ) )
 	define( 'VALUATOR_PLUGIN_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
 
-//require_once( 'valuator-functions.php' );
 require_once( 'classes/class-valuator.php' );
 
 global $valuator;
 $valuator = new ColdTurkey\Valuator\Valuator( __FILE__ );
 
-if( is_admin() ) {
+if ( is_admin() ) {
 	require_once( 'classes/class-valuator-admin.php' );
 	$valuator_admin = new ColdTurkey\Valuator\Valuator_Admin( __FILE__ );
 }
-
-?>
