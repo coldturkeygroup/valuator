fce893c38e8576d8f2961e490f907784d5821c52
Aaron Huisinga <aaron@platform.marketing>
2016-12-06 02:01:35 -0600

    Bump to version 1.14.3
    1. Update Zillow API class to work with Guzzle 6.0+

--- classes/class-zillow.php
+++ classes/class-zillow.php
@@ -2,7 +2,9 @@
 
 namespace ColdTurkey\Valuator;
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit;
+} // Exit if accessed directly.
 
 // Composer autoloader
 require_once VALUATOR_PLUGIN_PATH . 'vendor/autoload.php';
@@ -10,113 +12,113 @@ require_once VALUATOR_PLUGIN_PATH . 'vendor/autoload.php';
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 
-class Zillow {
+class Zillow
+{
 
-	protected $api_key;
-	protected $api_base;
-	protected $guzzle;
+    protected $api_key;
+    protected $api_base;
+    protected $guzzle;
 
-	/**
-	 * Basic constructor for the FrontDesk class
-	 *
-	 */
-	public function __construct()
-	{
-		$this->api_key  = get_option( 'pf_valuator_zillow_key' );
-		$this->api_base = 'https://www.zillow.com/webservice/';
-		$this->guzzle   = new Client();
-	}
+    /**
+     * Basic constructor for the FrontDesk class
+     *
+     */
+    public function __construct()
+    {
+        $this->api_key = get_option('pf_valuator_zillow_key');
+        $this->api_base = 'https://www.zillow.com/webservice/';
+        $this->guzzle = new Client();
+    }
 
-	/**
-	 * Get a Zillow Zestimate (estimated value)
-	 * for the defined address.
-	 *
-	 * @param string $address
-	 *
-	 * @return array|string
-	 */
-	public function getZestimate( $address )
-	{
-		try {
-			$address = $this->formatAddress( $address );
+    /**
+     * Get a Zillow Zestimate (estimated value)
+     * for the defined address.
+     *
+     * @param string $address
+     *
+     * @return array|string
+     */
+    public function getZestimate($address)
+    {
+        try {
+            $address = $this->formatAddress($address);
 
-			if ( $this->api_key != null || $this->api_key != '' ) {
-				$response = $this->guzzle->get( $this->api_base . 'GetSearchResults.htm', [
-					'query' => [
-						'zws-id'       => $this->api_key,
-						'address'      => $address[0],
-						'citystatezip' => $address[1]
-					]
-				] );
+            if ($this->api_key != null || $this->api_key != '') {
+                $response = $this->guzzle->get($this->api_base . 'GetSearchResults.htm', [
+                    'query' => [
+                        'zws-id'       => $this->api_key,
+                        'address'      => $address[0],
+                        'citystatezip' => $address[1]
+                    ]
+                ]);
 
-				$response = $response->xml();
+                $response = simplexml_load_string($response->getBody());
 
-				// Check to make sure we have a result
-				if ( ! $response->response ) {
-					$citystate = explode( ', ', $response->request->citystatezip );
+                // Check to make sure we have a result
+                if (!$response->response) {
+                    $city_state = explode(', ', $response->request->citystatezip);
 
-					return [
-						'error'  => 'no result',
-						'street' => (string) $response->request->address,
-						'city'   => (string) $citystate[0],
-						'state'  => (string) $citystate[1]
-					];
-				}
+                    return [
+                        'error'  => 'no result',
+                        'street' => (string) $response->request->address,
+                        'city'   => (string) $city_state[0],
+                        'state'  => (string) $city_state[1]
+                    ];
+                }
 
-				return $this->formatZestimate( $response );
-			}
-		}
-		catch ( RequestException $e ) {
-			return 'error';
-		}
-	}
+                return $this->formatZestimate($response);
+            }
+        } catch (RequestException $e) {
+            return 'error';
+        }
+    }
 
-	/**
-	 * Format the given address to be used
-	 * in the retrieval of the Zillow Zestimate
-	 *
-	 * @param string $address
-	 *
-	 * @return array
-	 */
-	protected function formatAddress( $address )
-	{
-		$formatted_address = [ ];
-		$address_array     = explode( ', ', $address );
+    /**
+     * Format the given address to be used
+     * in the retrieval of the Zillow Zestimate
+     *
+     * @param string $address
+     *
+     * @return array
+     */
+    protected function formatAddress($address)
+    {
+        $formatted_address = [];
+        $address_array = explode(', ', $address);
 
-		if ( count( $address_array ) == 4 ) {
-			// We have a full address, or at least a specific street
-			array_push( $formatted_address, $address_array[0] );
-			array_push( $formatted_address, $address_array[1] . ', ' . $address_array[2] );
-		}
+        if (count($address_array) == 4) {
+            // We have a full address, or at least a specific street
+            array_push($formatted_address, $address_array[0]);
+            array_push($formatted_address, $address_array[1] . ', ' . $address_array[2]);
+        }
 
-		return $formatted_address;
-	}
+        return $formatted_address;
+    }
 
-	/**
-	 * Format the data returned from Zillow
-	 * to be returned to the Valuator page.
-	 *
-	 * @param $response
-	 *
-	 * @return array
-	 */
-	protected function formatZestimate( $response )
-	{
-		$address   = $response->response->results->result->address;
-		$zestimate = $response->response->results->result->zestimate;
+    /**
+     * Format the data returned from Zillow
+     * to be returned to the Valuator page.
+     *
+     * @param $response
+     *
+     * @return array
+     */
+    protected function formatZestimate($response)
+    {
+        $address = $response->response->results->result->address;
+        $zestimate = $response->response->results->result->zestimate;
 
-		return [
-			'amount'   => '$' . number_format( (float) $zestimate->amount ),
-			'low'      => '$' . number_format( (float) $zestimate->valuationRange->low ),
-			'high'     => '$' . number_format( (float) $zestimate->valuationRange->high ),
-			'address'  => $address->street . ', ' . $address->city . ', ' . $address->state . ' ' . $address->zipcode,
-			'street'   => (string) $address->street,
-			'city'     => (string) $address->city,
-			'state'    => (string) $address->state,
-			'zip_code' => (string) $address->zipcode
-		];
+        return [
+            'amount'   => '$' . number_format((float) $zestimate->amount),
+            'low'      => '$' . number_format((float) $zestimate->valuationRange->low),
+            'high'     => '$' . number_format((float) $zestimate->valuationRange->high),
+            'address'  => $address->street . ', ' . $address->city . ', ' . $address->state . ' ' . $address->zipcode,
+            'street'   => (string) $address->street,
+            'city'     => (string) $address->city,
+            'state'    => (string) $address->state,
+            'zip_code' => (string) $address->zipcode
+        ];
 
-	}
+    }
 
 }
